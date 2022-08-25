e02f027683ab77729e84eff68a246d8189f50d55
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-08-25 20:16:09 -0300

    Create 0_hello_world.dart

---
+++ 0-Aprendendo_Dart/0_hello_world.dart
@@ -0,0 +1,29 @@
+void main() {
+  
+  //isso é um comentário
+  
+  /*
+   * Isso é um bloco de
+   * comentário!
+   * 
+   * Muito legal, não!
+   * 
+   * */
+  
+  
+  //para imprimir no "console":
+  print("Olá mundão véio!"); // <-
+  
+  print('Impressões com aspas simples também funcionam');
+  
+  //ATENÇÃO: BOAS PRÁTICAW DE PROGRAMAÇÃO: SE COMEÇAR COM UM PADRÃO, MANTENHA-O EM SEU CÓDIGO INTEIRO!
+  
+  print("O nome do xará\ndo professor é \"Thiago\"");
+  
+  print("\tEste é um texto tabulado");
+  
+  /*
+   * Usaremos prints em nosso código-fonte apenas 
+   * para depuração e testes
+   * */
+}
