1cc2baf438c3ece3cd92c74bddf92f4da0237487
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-10-13 22:00:54 -0300

    Primeiro commit do projeto - Iniciação

---
+++ todo_list/lib/theme/dark_theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+ThemeData darkTheme() {
+  return ThemeData(
+    colorScheme: const ColorScheme.dark(
+      primary: Colors.black,
+      secondary: Colors.orange
+    ),
+    brightness: Brightness.dark,
+    appBarTheme: const AppBarTheme(color: Colors.white12),
+    scaffoldBackgroundColor: Colors.white30,
+    iconTheme: const IconThemeData(color: Colors.white),
+    hintColor: Colors.orange,
+    floatingActionButtonTheme:
+      const FloatingActionButtonThemeData(backgroundColor: Colors.white70)
+  );
+}
---
+++ todo_list/lib/theme/light_theme.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightTheme() {
+  return ThemeData(
+      colorScheme: const ColorScheme.light(
+          primary: Colors.purple,
+          secondary: Colors.orange
+      ),
+      brightness: Brightness.light,
+      appBarTheme: const AppBarTheme(color: Colors.pinkAccent),
+      scaffoldBackgroundColor: Colors.grey[100],
+      iconTheme: const IconThemeData(color: Colors.white),
+      hintColor: Colors.orange,
+      floatingActionButtonTheme:
+      const FloatingActionButtonThemeData(backgroundColor: Colors.orange)
+  );
+}
