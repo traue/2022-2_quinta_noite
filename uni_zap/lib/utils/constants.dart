914403313bd55f4fb1cacb267d8bbc2273089e39
Prof. Thiago G. Traue <t.traue@gmail.com>
2022-11-18 11:30:23 -0300

    Continuação

---
+++ uni_zap/lib/utils/constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  //colors
+  static const kPrimaryColor = Color(0xFFFFFFFF);
+  static const kGreyColor = Color(0xFFEEEEEE);
+
+  //text
+  static const title = 'UniZap';
+  static const textSignInTitle = 'Seja bem-vindo(a)';
+
+  //navigation
+  static const signInNavigate = '/sign-in';
+  static const homeNavigate = '/home';
+}
